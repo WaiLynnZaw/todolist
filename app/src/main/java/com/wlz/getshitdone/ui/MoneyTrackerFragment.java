b2f8212c6cc65c34f6a6cd7b8ee735ee44a1b39a
WaiLynnZaw <kolynn1500@gmail.com>
2015-04-19 21:09:14 +0630

    Added Empty Fragment

---
+++ app/src/main/java/com/wlz/getshitdone/ui/MoneyTrackerFragment.java
@@ -0,0 +1,35 @@
+package com.wlz.getshitdone.ui;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wlz.getshitdone.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import tr.xip.errorview.ErrorView;
+
+/**
+ * Created by WaiLynnZaw on 4/19/15.
+ */
+public class MoneyTrackerFragment extends Fragment {
+    @InjectView(R.id.error_view)
+    ErrorView errorView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_vitamins,container,false);
+        ButterKnife.inject(this, v);
+        errorView.setImageResource(R.mipmap.ic_launcher);
+        errorView.setTitle("Coming Soon!");
+        errorView.setSubtitle("");
+        errorView.setTitleColor(Color.parseColor("#a3a3a3"));
+        errorView.showRetryButton(false);
+        return v;
+    }
+}
